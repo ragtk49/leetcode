4d95ca819756304198332521123b1eb1ad4535c0
Venkata Raghu <77735586+ragtk49@users.noreply.github.com>
2023-12-25 13:49:48 -0500

    Time: 7 ms (12.58%), Space: 45.1 MB (10.48%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,36 +14,26 @@
  * }
  */
 class Solution {
-    Map<Integer, Integer> map = new HashMap<>();
-    int index = 0;
-    int[] preorder_arr;
-    int[] inorder_arr;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        //Define variables
-        preorder_arr = preorder;
-        inorder_arr = inorder;
-        //Define the table
+        if(preorder == null || preorder.length == 0) return null;
+        int rootVal = preorder[0];
+        TreeNode root = new TreeNode(rootVal);
+        
+        int rootIdx = -1;
         for(int i = 0; i < inorder.length; i++){
-            map.put(inorder[i], i);
+            if(rootVal == inorder[i]){
+                rootIdx = i;
+                break;
+            }
         }
-        //Pass the boundaries
-        return dfs(0, inorder.length - 1);
-    }
-    private TreeNode dfs(int left, int right){
-        int curVal = preorder_arr[index++];
-        int curIdx = map.get(curVal);
-        //Build current node
-        TreeNode root = new TreeNode(curVal);
-            
-        //Build left subtree
-        if(left < curIdx){
-            root.left = dfs(left, curIdx - 1);
-        }
-        //Build right subtree
-        if(curIdx < right){
-            root.right = dfs(curIdx + 1, right);
-        }
-        //Return current root node
+        int inLeft[] = Arrays.copyOfRange(inorder, 0, rootIdx);
+        int inRight[] = Arrays.copyOfRange(inorder, rootIdx + 1, inorder.length);
+        int preLeft[] = Arrays.copyOfRange(preorder, 1, rootIdx + 1);
+        int preRight[] = Arrays.copyOfRange(preorder, rootIdx + 1, preorder.length);
+        
+        root.left = buildTree(preLeft, inLeft);
+        root.right = buildTree(preRight, inRight);
+        
         return root;
     }
 }
