d30c220fb5f2f945149c5da5f55a6a992974e8f9
Venkata Raghu <77735586+ragtk49@users.noreply.github.com>
2023-12-26 22:38:05 -0500

    Time: 0 ms (100.00%), Space: 40.7 MB (14.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums == null) return -1;
         int skip = 0;
         int take = nums[0];
         
         for(int i = 1; i < nums.length; i++){
             int temp = skip;
             
-            skip = Math.max(skip, take);
+            skip = Math.max(take, skip);
             
             take = temp + nums[i];
         }
-        
         return Math.max(skip, take);
     }
     
