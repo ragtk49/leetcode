b4351aa881cf232764b937525e24bf333053e418
Venkata Raghu <77735586+ragtk49@users.noreply.github.com>
2023-12-28 14:44:20 -0500

    Time: 0 ms (100.00%), Space: 40 MB (15.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,11 +2,11 @@ class Solution {
     public int climbStairs(int n) {
         int[] cache = new int[n + 1];
         cache[n] = 1;
-        cache[n-1] = 2;
+        cache[n-1] = 1;
         for(int i = n-2; i >=0 ; i--){
             cache[i] = cache[i+1] + cache[i+2];
         }
         
-        return cache[1];
+        return cache[0];
     }
 }
