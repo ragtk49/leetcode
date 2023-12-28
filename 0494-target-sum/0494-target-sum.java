f0ccf6946458efe253d58bb314b5b397622ddb41
Venkata Raghu <77735586+ragtk49@users.noreply.github.com>
2023-12-28 13:01:36 -0500

    Time: 462 ms (34.71%), Space: 45.8 MB (5.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -2,23 +2,31 @@ class Solution {
     int[] nums;
     int count;
     int target;
+    int[][] cache;
+    int total;
     public int findTargetSumWays(int[] nums, int target) {
         this.nums = nums;
         this.target = target;
+        total = Arrays.stream(nums).sum();
+        cache = new int[nums.length + 1][2 * total + 1];
+        
         if(nums == null) return 0;
         return helper(0, 0);
     }
     private int helper(int index, int sum){
         //base
-        // if(index > nums.length) return 0;
         if(index == nums.length){
-            if(sum == target) count++;
+           return  (sum == target) ? 1 : 0;
         }
-        else{
+        
+        if(cache[index][sum + total] != 0) return cache[index][sum + total]; 
+        
         //logic
         int add = helper(index + 1, sum + nums[index]);
         int delete = helper(index + 1, sum - nums[index]);
-        }
-        return count;
+        
+        cache[index][sum + total] = add + delete;
+        
+        return cache[index][sum + total];
     }
 }
